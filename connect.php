a902aa5b50ed1040a5728b6e3132d8a195094b3e
pradeeptishrivastava2308-source <pradeeptishrivastava2308@gmail.com>
2026-05-10 10:26:52 +0530

    connect.php

--- connect.php
+++ connect.php
@@ -1,8 +1,8 @@
 <?php
-$hostName = "sql111.ezyro.com";
-$dbuser = "ezyro_41875440";
-$dbpassword = "Pradeepti@123";
-$dbName = "ezyro_41875440_login_register_db";
+$hostName = "localhost";
+$dbuser = "root";
+$dbpassword = "";
+$dbName = "login_register";
 $conn=mysqli_connect($hostName,$dbuser,$dbpassword, $dbName);
 if(!$conn){
     die("Something went wrong");
